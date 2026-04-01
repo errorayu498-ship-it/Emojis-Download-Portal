2154956fd65e969a8b70e334c618ceb45c88ad5a
errorayu498-ship-it <errorayu498@gmail.com>
2026-04-01 18:38:37 +0500

    Fix filename handling in zip creation

--- download_zip.php
+++ download_zip.php
@@ -3,6 +3,7 @@
 $urls=$_POST["urls"];
 
 $zip=new ZipArchive();
+
 $file="cache/emojis.zip";
 
 $zip->open($file,ZipArchive::CREATE);
@@ -10,9 +11,8 @@ $zip->open($file,ZipArchive::CREATE);
 foreach($urls as $url){
 
 $data=file_get_contents($url);
-$name=basename($url);
 
-$zip->addFromString($name,$data);
+$zip->addFromString(basename($url),$data);
 
 }
 
