9fcb741b0308c42ae064093c0eee85b050151c8f
errorayu498-ship-it <errorayu498@gmail.com>
2026-04-01 18:35:59 +0500

    Refactor emoji loading and rendering functions

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,92 @@
-async function getEmojis(){
+let emojiList=[]
 
-let input=document.getElementById("serverInput").value
+async function loadServer(){
 
-document.getElementById("loading").classList.remove("hidden")
+let invite=document.getElementById("serverInput").value
 
-let res=await fetch("fetch_emojis.php?server="+encodeURIComponent(input))
+let bar=document.getElementById("progressBar")
+bar.style.width="20%"
+
+let res=await fetch("api.php?invite="+invite)
+
+bar.style.width="60%"
+
 let data=await res.json()
 
-document.getElementById("loading").classList.add("hidden")
+emojiList=data.emojis
 
-let container=document.getElementById("emojiContainer")
-container.innerHTML=""
+bar.style.width="100%"
 
-data.forEach(e=>{
+renderServer(data)
 
-let card=document.createElement("div")
-card.className="emoji-card"
+renderEmojis(data.emojis)
 
-card.innerHTML=`
-<img src="${e.url}">
-<p>${e.id}</p>
-<a href="${e.url}" download>
-<button>Download</button>
-</a>
+}
+
+function renderServer(data){
+
+document.getElementById("serverInfo").innerHTML=`
+
+<div class="serverCard">
+
+<img src="https://cdn.discordapp.com/icons/${data.id}/${data.icon}.png">
+
+<h2>${data.name}</h2>
+
+</div>
+
 `
 
-container.appendChild(card)
+}
+
+function renderEmojis(emojis){
+
+let grid=document.getElementById("emojiGrid")
+
+grid.innerHTML=""
+
+emojis.forEach(e=>{
+
+let ext=e.animated?"gif":"png"
+
+let url=`https://cdn.discordapp.com/emojis/${e.id}.${ext}`
+
+let card=document.createElement("div")
+
+card.className="emojiCard"
+
+card.innerHTML=`
+
+<img src="${url}" onclick="preview('${url}')">
+
+<p>${e.id}</p>
+
+<a href="${url}" download>
+<button>Download</button>
+</a>
+
+`
+
+grid.appendChild(card)
 
 })
 
-document.getElementById("zipBox").classList.remove("hidden")
+}
+
+function filterEmoji(){
+
+let text=document.getElementById("searchEmoji").value.toLowerCase()
+
+let filtered=emojiList.filter(e=>e.name.toLowerCase().includes(text))
+
+renderEmojis(filtered)
+
+}
+
+function preview(url){
+
+let win=window.open()
+
+win.document.write(`<img src="${url}" style="width:200px">`)
 
 }
